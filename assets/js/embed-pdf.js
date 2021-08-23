0e4383910a9fdad43bc3d9fa2f412bdfa3c24d08
Jessica Peters <fullerenedream@gmail.com>
2021-08-22 22:40:53 -0600

    adding js for new simple resume

---
+++ assets/js/embed-pdf.js
@@ -0,0 +1,55 @@
+/*
+Copyright 2019 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe.
+*/
+
+/* Pass the embed mode and viewer customization here */
+var viewerConfig = {
+    embedMode: "IN_LINE",
+    showDownloadPDF: true,
+    showPrintPDF: false,
+};
+
+/* Wait for Adobe Document Services PDF Embed API to be ready */
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    /* Initialize the AdobeDC View object */
+    var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
+        clientId: "5d4d1f8df8e14ddc840ac7d04837119d",
+        /* Pass the div id in which PDF should be rendered */
+        divId: "adobe-dc-view",
+    });
+
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+                // url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf",
+                url: "JessicaPeters_resume.pdf",
+                /*
+                If the file URL requires some additional headers, then it can be passed as follows:-
+                header: [
+                    {
+                        key: "<HEADER_KEY>",
+                        value: "<HEADER_VALUE>",
+                    }
+                ]
+                */
+            },
+        },
+        /* Pass meta data of file */
+        metaData: {
+            /* file name */
+            // fileName: "Bodea Brochure.pdf"
+            fileName: "JessicaPeters_resume.pdf"
+        }
+    }, viewerConfig);
+});
